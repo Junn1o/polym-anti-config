49134e54f544b0a0f78904fa7f3e2120d8b71251
Junnio <nphan218@gmail.com>
2026-01-29 23:46:51 +0700

    bring back reload command

--- src/main/java/com/junnio/anticonfig/command/ReloadCommand.java
+++ src/main/java/com/junnio/anticonfig/command/ReloadCommand.java
@@ -3,19 +3,22 @@ package com.junnio.anticonfig.command;
 import com.junnio.anticonfig.config.ModConfig;
 import com.mojang.brigadier.Command;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.permissions.Permissions;
 
-//public class ReloadCommand {
-//    public static void register() {
-//        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-//            dispatcher.register(CommandManager.literal("anticonfig")
-//                    .then(CommandManager.literal("reload")
-//                            .requires(source -> source.hasPermissionLevel(2)) // Requires operator permission
-//                            .executes(context -> {
-//                                ModConfig.load();
-//                                context.getSource().sendFeedback(() -> Text.literal("Configuration reloaded"), true);
-//                                return Command.SINGLE_SUCCESS;
-//                            })));
-//        });
-//    }
-//}
+public class ReloadCommand {
+    public static void register() {
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            dispatcher.register(Commands.literal("anticonfig")
+                    .then(Commands.literal("reload")
+                            .requires(source -> source.permissions().hasPermission(Permissions.COMMANDS_MODERATOR)) // Requires operator permission
+                            .executes(context -> {
+                                ModConfig.load();
+                                context.getSource().sendSuccess(() -> Component.literal("Configuration reloaded"), true);
+                                return Command.SINGLE_SUCCESS;
+                            })));
+        });
+    }
+}
 
